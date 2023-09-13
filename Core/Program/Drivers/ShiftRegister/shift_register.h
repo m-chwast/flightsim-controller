6fdb3b4f8df2c1f67bb1a090e1ebe54aa1490e77
Mateusz Chwast <chwastmat@gmail.com>
2023-09-14 01:05:43 +0200

    add outputs to sr and create test sr object

--- Core/Program/Drivers/ShiftRegister/shift_register.h
+++ Core/Program/Drivers/ShiftRegister/shift_register.h
@@ -8,17 +8,21 @@
 #pragma once
 
 #include "ShiftRegister/hc595.h"
-#include "gpio.h"
+#include "GPIO/gpio_pin.h"
 
 namespace Drivers {
 
 template <uint8_t bytesCount>
 class ShiftRegister {
+
 private:
 	Hardware::HC595 _register[bytesCount];
 
-public:
+	Hardware::GPIO_Output _srEnable;
+	Hardware::GPIO_Output _srClear;
+	Hardware::GPIO_Output _srStoreOutput;
 
+public:
 
 };
 
--- Core/Program/Init/init.cpp
+++ Core/Program/Init/init.cpp
@@ -1,6 +1,7 @@
 #include "init.h"
 
+#include "ShiftRegister/shift_register.h"
 
 void Init_CreateTasks(void) {
-
+	Drivers::ShiftRegister<2> testSR;
 }
