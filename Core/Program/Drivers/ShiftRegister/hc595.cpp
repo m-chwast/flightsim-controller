5e9cc302defe73cf3fa53846263689edfc1c9492
Mateusz Chwast <chwastmat@gmail.com>
2023-09-14 00:25:41 +0200

    add hc595 driver files

---
+++ Core/Program/Drivers/ShiftRegister/hc595.cpp
@@ -0,0 +1,9 @@
+/*
+ * hc595.cpp
+ *
+ *  Created on: Sep 14, 2023
+ *      Author: Mateusz Chwast
+ */
+
+#include "ShiftRegister/hc595.h"
+
---
+++ Core/Program/Drivers/ShiftRegister/hc595.h
@@ -0,0 +1,19 @@
+/*
+ * hc595.h
+ *
+ *  Created on: Sep 14, 2023
+ *      Author: Mateusz Chwast
+ */
+
+#pragma once
+
+#include <cstdint>
+
+class Driver_HC595 {
+private:
+	uint8_t output = 0;
+
+
+public:
+
+};
