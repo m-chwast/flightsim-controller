d032c86a74958d7940ccad8b52d96d5794298e04
Mateusz Chwast <chwastmat@gmail.com>
2023-10-08 01:00:59 +0200

    add nullcheck in gpio wrapper

--- Core/Program/Drivers/GPIO/gpio_pin.h
+++ Core/Program/Drivers/GPIO/gpio_pin.h
@@ -16,20 +16,33 @@ private:
 	GPIO_TypeDef* _gpio;
 	uint16_t _pin;
 
+	bool IsOperational() const {
+		return _gpio != nullptr;
+	}
+
 public:
 
 	GPIO_Output(GPIO_TypeDef* gpio, uint16_t pin)
 		: _gpio{gpio}, _pin{pin} {}
 
 	void SetLow() const {
+		if(!IsOperational()) {
+			return;
+		}
 		HAL_GPIO_WritePin(_gpio, _pin, GPIO_PIN_SET);
 	}
 
 	void SetHigh() const {
+		if(!IsOperational()) {
+			return;
+		}
 		HAL_GPIO_WritePin(_gpio, _pin, GPIO_PIN_RESET);
 	}
 
 	void Toggle() const {
+		if(!IsOperational()) {
+			return;
+		}
 		HAL_GPIO_TogglePin(_gpio, _pin);
 	}
 };
