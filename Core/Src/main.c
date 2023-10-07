4da8f4479cb1acc0957ea5b8347b712e387f8d8b
Mateusz Chwast <chwastmat@gmail.com>
2023-10-08 00:23:29 +0200

    setup spi2 as test spi for sr

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -19,6 +19,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "cmsis_os.h"
+#include "spi.h"
 #include "tim.h"
 #include "usart.h"
 #include "gpio.h"
@@ -91,6 +92,7 @@ int main(void)
   MX_GPIO_Init();
   MX_USART2_UART_Init();
   MX_TIM5_Init();
+  MX_SPI2_Init();
   /* USER CODE BEGIN 2 */
 
   Init_CreateTasks();
