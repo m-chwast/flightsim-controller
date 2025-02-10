4a6f5b47d288a3f8403c6508a057b7f9b8a6cc27
Mateusz Chwast <chwastmat@gmail.com>
2025-02-10 23:29:14 +0100

    add cmake preload file to automatically set generator

---
+++ PreLoad.cmake
@@ -0,0 +1,13 @@
+cmake_minimum_required(VERSION 3.20)
+
+if (CMAKE_BUILD_TYPE STREQUAL "Debug" OR CMAKE_BUILD_TYPE STREQUAL "Release")
+
+set (CMAKE_GENERATOR "Unix Makefiles" CACHE INTERNAL "" FORCE)
+
+elseif (CMAKE_BUILD_TYPE STREQUAL "TestDebug")
+
+set (CMAKE_GENERATOR "MinGW Makefiles" CACHE INTERNAL "" FORCE)
+
+endif()
+
+message ("PreLoad.cmake -- Set generator to ${CMAKE_GENERATOR}")
